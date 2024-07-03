03a4ddb82459e68c3d1a5bfcd81ed8e4eea2da20
nikolay_cholakov <nikolaicholakov25@gmail.com>
2024-07-03 16:04:22 +0300

    add some basic mobile styles

--- src/components/layouts/basic/index.tsx
+++ src/components/layouts/basic/index.tsx
@@ -2,13 +2,19 @@ import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 
 import { Header } from '@components/header';
+import { cn } from '@utils/helpers';
 
 export const LayoutBasic = () => {
   return (
     <div className="flex h-screen w-screen flex-col items-stretch justify-stretch">
       <Header />
       <Suspense>
-        <div className="flex flex-1 flex-col px-14 pb-16 pt-8">
+        <div className={cn(
+          'flex flex-1 flex-col',
+          'lg:px-14 lg:pb-16 lg:pt-8',
+          'px-6 pb-8 pt-4',
+        )}
+        >
           <Outlet />
         </div>
       </Suspense>
--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -3,22 +3,34 @@ import { cn } from '@utils/helpers';
 
 const Home = () => {
   return (
-    <section className={cn('flex flex-1 flex-col items-center pb-[10.75rem] pt-[8rem]')}>
-      <h1 className={cn('pb-4 text-center text-h2-bold')}> 
-        <span className="pr-2 text-h1-bold text-dev-pink-500">
+    <section className={cn(
+      'flex flex-1 flex-col items-center',
+      'lg:pb-[10.75rem] lg:pt-[8rem]',
+    )}
+    >
+      <h1 className={cn(
+        'pb-4 text-center text-h3-bold',
+        'lg:text-h2-bold',
+      )}
+      > 
+        <span className="hidden pr-2 text-h1-bold text-dev-pink-500 lg:inline">
         [
         </span>
           Empower Your Polkadot Development
-        <span className="pl-2 text-h1-bold text-dev-pink-500">
+        <span className="hidden pl-2 text-h1-bold text-dev-pink-500 lg:inline">
         ]
         </span>
       </h1>
-      <p className={cn('pb-20 text-center text-h5-regular')}>Elevate your development experience with our next-gen platform for Polkadot developers</p>
+      <p className={cn('pb-14 text-center text-body1-regular lg:pb-20 lg:text-h5-regular')}>Elevate your development experience with our next-gen platform for Polkadot developers</p>
 
-      <div className={cn('grid w-full max-w-[880px] grid-cols-2 gap-4 [&>*]:min-h-[320px]')} > 
+      <div className={cn(
+        'grid w-full max-w-[880px] grid-cols-1 gap-4',
+        'md:grid-cols-2 [&>*]:md:min-h-[320px]',
+      )}
+      > 
         <FeatureCard
           icon="icon-github"
-          link={
+          linkProps={
             { to: 'https://github.com/LimeChain/polkadot-devground-client',
               target: '_blank', 
             }}
@@ -27,7 +39,7 @@ const Home = () => {
         />
         <FeatureCard
           icon="icon-brackets"
-          link={{ to: '/code?s=1' }}
+          linkProps={{ to: '/code?s=1' }}
           title="Developer Console"
           subTitle="Dive into our advanced developer console for seamless on-chain interaction and rapid prototyping. Experience a streamlined, user-friendly environment designed to accelerate your development process and boost productivity."
         />
