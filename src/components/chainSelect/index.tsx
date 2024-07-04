7585dee2a854186fcc9f99e00de0321c41146e84
nikolay_cholakov <nikolaicholakov25@gmail.com>
2024-07-04 11:49:18 +0300

    remove gap

--- src/components/chainSelect/index.tsx
+++ src/components/chainSelect/index.tsx
@@ -2,7 +2,7 @@ import { Icon } from '@components/icon';
 
 const ChainSelect = () => {
   return (
-    <button type="button" className="flex items-center gap-2">
+    <button type="button" className="flex items-center">
       <Icon name="icon-chain-polkadot" size={[28]}/>
       <h5 className="ml-[6px] mr-3 text-h5-bold">Polkadot</h5>
       <Icon name="icon-dropdownArrow" size={[16]}/>
