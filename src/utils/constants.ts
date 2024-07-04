c81b9c70dd97667d0f5aa2781a96b0de2c7b0180
nikolay_cholakov <nikolaicholakov25@gmail.com>
2024-07-04 16:54:55 +0300

    create chains constant

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,30 @@
 export const GITHUB_REPO_LINK = 'https://github.com/LimeChain/polkadot-devground-client';
 export const X_LINK = 'https://x.com/LimeChainHQ';
 export const LIMECHAIN_LINK = 'https://limechain.tech/';
+
+export const CHAINS = [
+  {
+    'polkadot': {
+      name: 'Polkadot & Parachains',
+      chains: [
+        {
+          name: 'Polkadot',
+          icon: 'icon-chain-polkadot',
+        },
+        {
+          name: 'Astar',
+          icon: 'icon-chain-astar',
+        },
+      ],
+    }, 
+    'rococo': {
+      name: 'Rococo & Parachains',
+      chains: [
+        {
+          name: 'Rococo',
+          icon: 'icon-chain-rococo',
+        },
+      ],
+    }, 
+  },
+];
