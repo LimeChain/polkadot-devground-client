9b71e56cf89882868b16e23c812cb5ef95079b59
Pavel Ivanov <pavel.ivanov@limechain.tech>
2024-07-05 13:45:57 +0300

    initial setup

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,12 +1,33 @@
-import { useCallback } from 'react';
+import {
+  useCallback,
+  useEffect,
+} from 'react';
 import { Link } from 'react-router-dom';
 
 import { Icon } from '@components/icon';
+import { useStoreUI } from '@stores';
 import { useTheme } from '@utils/hooks/useTheme';
 
 export const Header = () => {
-
   const { isDarkTheme, changeTheme } = useTheme();
+
+  const initStoreUI = useStoreUI.use.init?.();
+  const {
+    resetStore: resetStoreUI,
+    countIncrement,
+    countDecrement,
+  } = useStoreUI.use.actions();
+
+  const count = useStoreUI.use.count?.();
+
+  useEffect(() => {
+    initStoreUI();
+
+    return () => {
+      resetStoreUI();
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const handleChangeTheme = useCallback(async () => {
     await changeTheme(isDarkTheme ? 'light' : 'dark');
@@ -17,6 +38,17 @@ export const Header = () => {
       <Link to="/" className="text-current hover:text-current">
         <Icon name="logo-polkadot" size={[128, 40]} />
       </Link>
+      <div>
+        <button type="button" onClick={countDecrement}>
+          -
+        </button>
+        <span className="mx-4 text-xl">
+          {count}
+        </span>
+        <button type="button" onClick={countIncrement}>
+          +
+        </button>
+      </div>
       <div className="flex">
         <button type="button" onClick={handleChangeTheme}>
           <Icon
