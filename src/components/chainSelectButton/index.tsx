65f0680cbef6a1e304b29647a35cf64b29b241d6
nikolay_cholakov <nikolaicholakov25@gmail.com>
2024-07-12 17:51:36 +0300

    turn header component into a display only

--- src/components/chainSelectButton/index.tsx
+++ src/components/chainSelectButton/index.tsx
@@ -1,15 +1,24 @@
 import { useToggleVisibility } from '@pivanov/use-toggle-visibility';
+import { useLocation } from 'react-router-dom';
 
 import { Icon } from '@components/icon';
 import { ModalChainSelect } from '@components/modals/modalChainSelect';
 import { useStoreChain } from '@stores';
 
 const ChainSelectButton = () => {
+
+  const { pathname } = useLocation();
+  const isHomePage = pathname === '/';
+
   const [
     ChainSelectModal,
     toggleVisibility,
   ] = useToggleVisibility(ModalChainSelect);
   const currentChain = useStoreChain.use.chain();
+
+  if (isHomePage) {
+    return null;
+  }
 
   return (
     <>
