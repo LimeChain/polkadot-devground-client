0a3c69ee9fe8875a4a906cdab66ca37406cd2e16
nikolay_cholakov <nikolaicholakov25@gmail.com>
2024-07-12 17:49:41 +0300

    extract toggleTheme in a component

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,40 +1,29 @@
 import { useLocation } from 'react-router-dom';
 
 import ChainSelectButton from '@components/chainSelectButton';
-import { Icon } from '@components/icon';
 import { Logo } from '@components/logo';
-import { useStoreUI } from '@stores';
-import { cn } from '@utils/helpers';
+import { Navigation } from '@components/navigation';
+import { ToggleTheme } from '@components/toggleTheme';
 
 export const Header = () => {
   const { pathname } = useLocation();
   const isHomePage = pathname === '/';
 
-  const { toggleTheme } = useStoreUI.use.actions();
-  const theme = useStoreUI.use.theme?.();
-
   return (
     <nav className="flex items-center justify-between px-6 ">
-      <div className="flex items-center">
+      <div className="flex items-center gap-12">
         <Logo />
+        {
+          !isHomePage
+          && <Navigation />
+        }
       </div>
       <div className="flex gap-5">
-        {!isHomePage && <ChainSelectButton/> }
-        <button
-          type="button"
-          onClick={toggleTheme}
-          className={cn(
-            'navSpacer',
-            { 'ml-5 ': !isHomePage },
-            { 'before:content-none': isHomePage },
-          )}
-        >
-          <Icon
-            name={theme ? 'icon-lightMode' : 'icon-darkMode'}
-            size={[24]}
-            className="text-dev-black-600 dark:text-dev-purple-100"
-          />
-        </button>
+        {
+          !isHomePage
+          && <ChainSelectButton />
+        }
+        <ToggleTheme />
       </div>
     </nav>
   );
