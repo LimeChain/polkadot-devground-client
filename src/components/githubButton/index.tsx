2948c032d27707d7807c7404a76bb060783d3192
Denis Dinkov <denisdinkov7@gmail.com>
2024-07-17 15:38:19 +0300

    [ui] - login github modal (#33)

--- src/components/githubButton/index.tsx
+++ src/components/githubButton/index.tsx
@@ -1,30 +1,40 @@
+import { useToggleVisibility } from '@pivanov/use-toggle-visibility';
+
 import { Icon } from '@components/icon';
+import { ModalGithubLogin } from '@components/modals/modalGithubLogin';
 import { useStoreAuth } from '@stores';
 import { cn } from '@utils/helpers';
 
 export const GithubButton = () => {
+  const [
+    GithubModal,
+    toggleVisibility,
+  ] = useToggleVisibility(ModalGithubLogin);
 
-  const { authorize, logout } = useStoreAuth.use.actions();
-  const isAuthenticated = useStoreAuth.use.jwtToken();
-  const authIsLoading = useStoreAuth.use.jwtTokenIsLoading();
+  const { logout } = useStoreAuth.use.actions();
+  const authIsLoading = useStoreAuth.use.jwtTokenIsLoading?.();
+  const isAuthenticated = useStoreAuth.use.jwtToken?.();
 
   return (
-    <button
-      type="button"
-      disabled={authIsLoading}
-      onClick={isAuthenticated ? logout : authorize}
-      className={cn(
-        'flex cursor-pointer items-center gap-1',
-        'disabled:cursor-not-allowed disabled:opacity-50',
-        'font-geist text-body1-bold',
-      )}
-    >
-      <Icon name="icon-github" />
-      {
-        isAuthenticated
-          ? 'Logout'
-          : 'Login'
-      }
-    </button>
+    <>
+      <button
+        type="button"
+        disabled={authIsLoading}
+        onClick={isAuthenticated ? logout : toggleVisibility}
+        className={cn(
+          'flex cursor-pointer items-center gap-1',
+          'disabled:cursor-not-allowed disabled:opacity-50',
+          'font-geist text-body1-bold',
+        )}
+      >
+        <Icon name="icon-github" />
+        {
+          isAuthenticated
+            ? 'Logout'
+            : 'Login'
+        }
+      </button>
+      <GithubModal onClose={toggleVisibility} />
+    </>
   );
 };
