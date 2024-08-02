03bbb978cad568d3db94b2739295c222fec37942
Pavel Ivanov <pavel.ivanov@limechain.tech>
2024-08-02 16:17:35 +0300

    [feature] - explorer (#43)

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,3 +1,4 @@
 export * from './ui';
 export * from './chain';
+export * from './explorer';
 export * from './auth';
--- src/types/block.ts
+++ src/types/block.ts
@@ -1,15 +1,14 @@
-export interface IBlockHeader {
-  number: string;
-  parentHash: string;
-  stateRoot: string;
-  extrinsicsRoot: string;
-  digest: {
-    logs: string[];
-  };
-}
-
-// Define the block type
 export interface IBlock {
-  header: IBlockHeader;
+  hash: string;
   extrinsics: string[];
+  header: {
+    number: string;
+    parentHash: string;
+    stateRoot: string;
+    extrinsicsRoot: string;
+    digest: {
+      logs: string[];
+    };
+  };
+  timestamp: Date | null;
 }
