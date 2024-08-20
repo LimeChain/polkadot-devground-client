a967f87801da8e26bb8bee8de1689bcc8d845cfd
nikolay_cholakov <nikolaicholakov25@gmail.com>
2024-08-20 18:23:13 +0300

    extract all papi methods into functions

--- src/utils/papi/helpers.ts
+++ src/utils/papi/helpers.ts
@@ -1,15 +1,11 @@
-import { CompatibilityLevel } from 'polkadot-api';
-
-export const checkForProp = (prop: any, message: string) => {
-  if (!prop) {
+export function assert(prop: unknown, message: string): asserts prop {
+  if (prop === null || typeof prop === 'undefined') {
     throw new Error(`${message} is not defined`);
   }
-};
+}
 
-export const checkIfCompatable = (method: { isCompatible: any }, message: string) => {
-  const isCompatable = method.isCompatible(CompatibilityLevel.Partial);
-
+export const checkIfCompatable = (isCompatable: boolean, message: string) => {
   if (!isCompatable) {
-    throw new Error(`${message} is not compatable`);
+    throw new Error(message);
   }
 };
