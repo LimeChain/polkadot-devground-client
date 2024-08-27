1ec8cbec166674969c8749c3175a338d31594e47
Denis Dinkov <denisdinkov7@gmail.com>
2024-08-27 17:38:06 +0300

    All Blocks Changes

--- src/views/latestBlocks/index.tsx
+++ src/views/latestBlocks/index.tsx
@@ -23,85 +23,88 @@ const LatestBlocks = () => {
   }, [navigate]);
 
   return (
-    <>
+    <div className="grid h-full grid-rows-[40px_46px_1fr] gap-6">
       <PageHeader title="Latest Blocks" />
-      <SearchBar
-        label="Search by Block"
-        classNames="mt-6"
-      />
+      <SearchBar label="Search by Block"/>
       <PDScrollArea
         className="table-container"
         verticalScrollClassNames="pt-8"
       >
         <table className="explorer-pages-table">
-          <thead>
-            <tr>
-              <th>Block</th>
-              <th>Status</th>
-              <th>Time</th>
-              <th>Extrinsics</th>
-              <th>Events</th>
-              <th>Validator</th>
-              <th>Block Hash</th>
-            </tr>
-          </thead>
-          <tbody>
-            {
-              isLoading
-                ? 'Loading...'
-                : (
-                  Array.from(blocksData.values()).reverse().map((block, _idx) => {
-                    const timeAgo = block.header.timestamp && formatDistanceToNowStrict(
-                      new Date(block.header.timestamp),
-                      { addSuffix: true },
-                    );
-                    const isFinalized = latestFinalizedBlock && latestFinalizedBlock >= block.header.number;
+          <colgroup>
+            <col style={{ width: '10%', minWidth: '8rem' }} />
+            <col style={{ width: '10%', minWidth: '4rem' }} />
+            <col style={{ width: '20%', minWidth: '8rem' }} />
+            <col style={{ width: '10%', minWidth: '6rem' }} />
+            <col style={{ width: '10%', minWidth: '6rem' }} />
+            <col style={{ width: '20%', minWidth: '10rem' }} />
+            <col style={{ width: '20%', minWidth: '10rem' }} />
+          </colgroup>
+          <tr className="table-head">
+            <th>Block</th>
+            <th>Status</th>
+            <th>Time</th>
+            <th>Extrinsics</th>
+            <th>Events</th>
+            <th>Validator</th>
+            <th>Block Hash</th>
+          </tr>
+          {
+            isLoading
+              ? 'Loading...'
+              : (
+                Array.from(blocksData.values()).reverse().map((block, _idx) => {
+                  const timeAgo = block.header.timestamp && formatDistanceToNowStrict(
+                    new Date(block.header.timestamp),
+                    { addSuffix: true },
+                  );
+                  const isFinalized = latestFinalizedBlock && latestFinalizedBlock >= block.header.number;
 
-                    return (
-                      <tr
-                        key={block.header.number}
-                        onClick={goRouteId}
-                        data-block-number={block.header.number}
-                        className={cn(
-                          {
-                            ['opacity-0 animate-fade-in animation-duration-500 animation-delay-500']: _idx === 0,
-                          },
-                        )}
-                      >
-                        <td>{block.header.number}</td>
-                        <td>
-                          {
-                            isFinalized
-                              ? (
-                                <Icon
-                                  size={[16]}
-                                  name="icon-checked"
-                                  className="text-dev-green-600"
-                                />
-                              )
-                              : (
-                                <Icon
-                                  size={[16]}
-                                  name="icon-clock"
-                                  className="text-dev-yellow-700"
-                                />
-                              )
-                          }
-                        </td >
-                        <td>{timeAgo}</td>
-                        <td>{block.body.extrinsics.length}</td>
-                        <td>{block.body.events.length}</td>
-                        <td>{truncateAddress(block.header.identity, 6)}</td>
-                        <td>{truncateAddress(block.header.hash, 6)}</td>
-                      </tr >
-                    );
-                  })
-                )
-            }
-          </tbody >
+                  return (
+                    <tr
+                      key={block.header.number}
+                      onClick={goRouteId}
+                      data-block-number={block.header.number}
+                      className={cn(
+                        'table-row cursor-pointer',
+                        {
+                          ['opacity-0 animate-fade-in animation-duration-500 animation-delay-500']: _idx === 0,
+                        },
+                      )}
+                    >
+                      <td>{block.header.number}</td>
+                      <td>
+                        {
+                          isFinalized
+                            ? (
+                              <Icon
+                                size={[16]}
+                                name="icon-checked"
+                                className="text-dev-green-600"
+                              />
+                            )
+                            : (
+                              <Icon
+                                size={[16]}
+                                name="icon-clock"
+                                className="text-dev-yellow-700"
+                              />
+                            )
+                        }
+                      </td >
+                      <td>{timeAgo}</td>
+                      <td>{block.body.extrinsics.length}</td>
+                      <td>{block.body.events.length}</td>
+                      <td>{truncateAddress(block.header.identity, 6)}</td>
+                      <td>{truncateAddress(block.header.hash, 6)}</td>
+                    </tr >
+                  );
+                })
+              )
+          }
         </table >
       </PDScrollArea >
-    </>
+    </div>
   );
 };
 
