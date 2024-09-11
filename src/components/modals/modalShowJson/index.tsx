1a7f09f36dbca93b24f9824ffc93472426b02c7d
Denis Dinkov <denisdinkov7@gmail.com>
2024-09-11 15:13:04 +0300

    [feat] Search by block and extrinsic (#59)

--- src/components/modals/modalShowJson/index.tsx
+++ src/components/modals/modalShowJson/index.tsx
@@ -20,8 +20,8 @@ export const ModalShowJson = (props: IModalShowJson) => {
     <Modal
       onClose={onClose}
       className={cn(
-        'w-5/6',
-        'flex max-h-screen flex-col gap-8 overflow-auto p-6',
+        'z-[999] w-5/6',
+        'flex flex-col gap-8 overflow-auto p-6',
         'border border-dev-purple-300',
         'dark:border-dev-purple-700',
         'transition-all duration-300 ease-in-out',
