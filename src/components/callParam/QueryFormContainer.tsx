0e911f3b0c20339a46c134fe43ecffe9eb359a73
Pavel Ivanov <pavel.ivanov@limechain.tech>
2024-09-11 13:47:06 +0300

    scroll area in rpc calls

--- src/components/callParam/QueryFormContainer.tsx
+++ src/components/callParam/QueryFormContainer.tsx
@@ -1,3 +1,5 @@
+import { useRef } from 'react';
+
 import { PDScrollArea } from '@components/pdScrollArea';
 
 import type React from 'react';
@@ -7,12 +9,19 @@ export interface IQueryFormContainer {
 }
 
 export const QueryFormContainer = ({ children }: IQueryFormContainer) => {
+  const refScrollArea = useRef<HTMLDivElement | null>(null);
+
   return (
     <PDScrollArea
-      className="pb-2 pr-2 first:only:border " /* add space for the scrollbar */
-      viewportClassNames="max-w-full w-full"
+      ref={refScrollArea}
+      verticalScrollClassNames="!bottom-0" // remove default bottom space because we need only a vertical scroll
     >
-      <div className="flex w-full flex-col gap-6">
+      <div
+        className="flex w-full max-w-full flex-col gap-6 pr-4"
+        style={{
+          width: refScrollArea.current?.clientWidth,
+        }}
+      >
         {children}
       </div>
     </PDScrollArea>
