81462a8c3b26362b105c41cee0cf490c63bd305b
Denis Dinkov <denisdinkov7@gmail.com>
2024-09-24 10:10:12 +0300

    [Feat] Home design (#66)

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -4,3 +4,42 @@ export const LINK_LIMECHAIN = 'https://limechain.tech/';
 
 export const LINK_POLKADOT_WIKI = 'https://wiki.polkadot.network/';
 export const LINK_POLKADOT_GITHUB = 'https://github.com/paritytech/polkadot-sdk';
+
+export const HOME_LINKS = [
+  {
+    to: '/code?s=1',
+    iconName: 'icon-brackets',
+    title: 'Developer Console',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget sapien. Sed ut purus eget sapien.',
+  },
+  {
+    to: '/explorer',
+    iconName: 'icon-explorer',
+    title: 'Explorer',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget sapien. Sed ut purus eget sapien.',
+  },
+  {
+    to: '/forks',
+    iconName: 'icon-forks',
+    title: 'Forks',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget sapien. Sed ut purus eget sapien.',
+  },
+  {
+    to: '/chain-state',
+    iconName: 'icon-chain',
+    title: 'Chain State',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget sapien. Sed ut purus eget sapien.',
+  },
+  {
+    to: '/rpc-calls',
+    iconName: 'icon-rpcCalls',
+    title: 'RPC Calls',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget sapien. Sed ut purus eget sapien.',
+  },
+  {
+    to: '/runtime-calls',
+    iconName: 'icon-runtimeCalls',
+    title: 'Runtime Calls',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget sapien. Sed ut purus eget sapien.',
+  },
+];
--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,5 +1,6 @@
 import { Icon } from '@components/icon';
 import { PDLink } from '@components/pdLink';
+import { HOME_LINKS } from '@constants/links';
 import { cn } from '@utils/helpers';
 
 const Home = () => {
@@ -17,94 +18,57 @@ const Home = () => {
           Empower Your Polkadot Development
         </h1>
 
-        <p className="pb-14 text-center font-body1-regular lg:pb-20 lg:font-h5-regular">
+        <p className={cn(
+          'pb-14',
+          'text-center font-body1-regular',
+          'lg:font-h5-regular" lg:pb-20',
+        )}
+        >
           Elevate your development experience with our next-gen platform for Polkadot developers
         </p>
 
-        <div className="grid w-full max-w-[880px] grid-cols-1 gap-4 md:grid-cols-2">
-          <PDLink
-            target="_blank"
-            to="https://github.com/LimeChain/polkadot-devground-client"
-            className={cn(
-              'group',
-              'relative',
-              'flex flex-1 flex-col',
-              'p-6',
-              'min-h-[320px]',
-              '!text-current',
-              'border border-transparent hover:border-dev-pink-500',
-              'transition-border-colors duration-200 ease-linear',
-              'bg-dev-purple-100 dark:bg-dev-black-900',
-
-              'after:pointer-events-none after:absolute after:opacity-0 after:content-[""]',
-              'after:-right-3/4 after:-top-3/4 after:size-3/4 after:bg-dev-purple-500 after:blur-[150px]',
-              'after:transition-opacity after:duration-200',
-              'hover:after:opacity-100',
-              'overflow-hidden',
-            )}
-          >
-            <Icon
-              name="icon-linkArrow"
-              size={[24]}
+        <div className="grid w-full grid-cols-1 gap-4 md:grid-cols-3">
+          {HOME_LINKS.map((card) => (
+            <PDLink
+              key={card.to}
+              to={card.to}
               className={cn(
-                'absolute right-4 top-4',
-                'transition-transform duration-200 ease-linear',
-                'group-hover:-translate-y-1 group-hover:translate-x-1',
+                'group relative flex flex-1 flex-col p-6 !text-current',
+                'border border-transparent hover:border-dev-pink-500',
+                'transition-all duration-200 ease-linear',
+                'bg-dev-purple-100 dark:bg-dev-black-900',
+                'overflow-hidden',
+                'after:pointer-events-none after:absolute after:opacity-0 after:content-[""]',
+                'after:-right-3/4 after:-top-3/4 after:size-3/4 after:bg-dev-purple-500 after:blur-[150px]',
+                'after:transition-opacity after:duration-200',
+                'hover:after:opacity-100',
               )}
-            />
-            <Icon
-              name="logo-github"
-              className="mb-8 text-dev-pink-500"
-              size={[40]}
-            />
-            <h4 className="mb-2 font-h4-bold">
-              Code
-            </h4>
-            <p className="font-geist text-dev-black-300 font-body2-regular dark:text-dev-purple-300">
-              Explore our GitHub repository for comprehensive documentation, code examples, and contributions. Join our developer community to enhance your Polkadot projects.
-            </p>
-          </PDLink>
-          <PDLink
-            to="/code?s=1"
-            className={cn(
-              'group',
-              'relative',
-              'flex flex-1 flex-col',
-              'p-6',
-              'min-h-[320px]',
-              '!text-current',
-              'border border-transparent hover:border-dev-pink-500',
-              'transition-border-colors duration-200 ease-linear',
-              'bg-dev-purple-100 dark:bg-dev-black-900',
-
-              'after:pointer-events-none after:absolute after:opacity-0 after:content-[""]',
-              'after:-right-3/4 after:-top-3/4 after:size-3/4 after:bg-dev-purple-500 after:blur-[150px]',
-              'after:transition-opacity after:duration-200',
-              'hover:after:opacity-100',
-              'overflow-hidden',
-            )}
-          >
-            <Icon
-              name="icon-linkArrow"
-              size={[24]}
-              className={cn(
-                'absolute right-4 top-4',
-                'transition-transform duration-200 ease-linear',
-                'group-hover:-translate-y-1 group-hover:translate-x-1',
+            >
+              <Icon
+                name="icon-linkArrow"
+                className={cn(
+                  'absolute right-4 top-4',
+                  'group-hover:-translate-y-1 group-hover:translate-x-1',
+                  'transition-transform duration-200 ease-linear',
+                )}
+              />
+              <Icon
+                name={card.iconName}
+                className="mb-7 text-dev-pink-500"
+                size={[40]}
+              />
+              <h4 className="mb-2 font-h4-bold">
+                {card.title}
+              </h4>
+              <p className={cn(
+                'font-geist text-dev-black-300 font-body2-regular',
+                'dark:text-dev-purple-300',
               )}
-            />
-            <Icon
-              name="icon-brackets"
-              className="mb-8 text-dev-pink-500"
-              size={[40]}
-            />
-            <h4 className="mb-2 font-h4-bold">
-              Developer Console
-            </h4>
-            <p className="font-geist text-dev-black-300 font-body2-regular dark:text-dev-purple-300">
-              Dive into our advanced developer console for seamless on-chain interaction and rapid prototyping. Experience a streamlined, user-friendly environment designed to accelerate your development process and boost productivity.
-            </p>
-          </PDLink>
+              >
+                {card.description}
+              </p>
+            </PDLink>
+          ))}
         </div>
       </div>
     </section>
