0422b40aeda10eb14ad80287b6d68a1151229467
Denis Dinkov <denisdinkov7@gmail.com>
2024-09-24 10:54:57 +0300

    Changes

--- src/components/modals/modalJSONViewer/index.tsx
+++ src/components/modals/modalJSONViewer/index.tsx
@@ -12,7 +12,7 @@ import type {
 } from '@custom-types/block';
 
 interface IModalJSONViewer extends Pick<IModal, 'onClose'> {
-  jsonData?: IMappedBlockExtrinsic | IMappedBlockEvent | unknown;
+  jsonData?: IMappedBlockExtrinsic | IMappedBlockEvent | null;
   title?: string;
   onClose: () => void;
 }
