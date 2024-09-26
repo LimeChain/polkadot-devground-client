6db5a4fed192b532b116b41f5e0aad2c2454f2ca
Denis Dinkov <denisdinkov7@gmail.com>
2024-09-26 10:50:51 +0300

    Update virtualizedList.tsx

--- src/views/forks/virtualizedList.tsx
+++ src/views/forks/virtualizedList.tsx
@@ -105,7 +105,7 @@ export const VirtualizedList = (props: IVirtualizedListProps) => {
   }, [items]);
 
   return (
-    <div className="disable-vertical-scroll flex h-full flex-col">
+    <div className="flex h-full flex-col">
       <div className="flex justify-between">
         <PageHeader title="Forks" />
         <ScrollButtons refScrollArea={refScrollArea} />
@@ -119,7 +119,7 @@ export const VirtualizedList = (props: IVirtualizedListProps) => {
           'px-8 py-20',
           'font-geist text-dev-purple-50 font-body2-regular',
         )}
-        className="disable-horizontal-scroll"
+        className="disable-horizontal-scroll disable-vertical-scroll"
       >
         <div
           ref={refContent}
