a250ebbf9c4ec9f78d6dca0dd48fea877db2e7c3
Denis Dinkov <denisdinkov7@gmail.com>
2024-10-15 13:41:18 +0300

    Load Examples

---
+++ src/stores/gists/index.ts
@@ -0,0 +1,70 @@
+import { busDispatch } from '@pivanov/event-bus';
+import { create } from 'zustand';
+
+import authService from '@services/authService';
+import gistService from '@services/gistService';
+import { createSelectors } from 'src/stores/createSelectors';
+
+import type { IEventBusMonacoEditorLoadSnippet } from '@custom-types/eventBus';
+
+interface StoreInterface {
+  gists: any[];
+  actions: {
+    resetStore: () => void;
+    uploadSnippet: () => void;
+    getSnippets: () => void;
+  };
+  init: () => Promise<void>;
+}
+
+const initialState: Omit<StoreInterface, 'actions' | 'init'> = {
+  gists: [],
+};
+
+const baseStore = create<StoreInterface>()((set) => ({
+  ...initialState,
+  actions: {
+    uploadSnippet: async () => {
+      try {
+        await gistService.uploadSnippet();
+      } catch (error) {
+        console.error('Error uploading snippet', error);
+        throw error;
+      }
+    },
+
+    getSnippets: async () => {
+      const data = await gistService.getUserGists();
+      set({ gists: data });
+    },
+
+    loadSnippet: async (id: string) => {
+
+      try {
+        const data = await gistService.getGistContent(id);
+
+        busDispatch<IEventBusMonacoEditorLoadSnippet>({
+          type: '@@-monaco-editor-load-snippet',
+          data,
+        });
+      } catch (error) {
+        console.error('Error loading snippet', error);
+        throw error;
+      }
+    },
+    resetStore: () => set({ ...initialState }),
+  },
+
+  init: async () => {
+    try {
+      const jwtToken = await authService.getJwtToken();
+    } catch (error) {
+      console.error('Error initializing auth store', error);
+      throw error;
+    }
+  },
+}));
+
+export const baseStoreGists = baseStore;
+export const useStoreGists = createSelectors(baseStore);
+
