b2b4b7dfa4610370809472342fb9d2b614440ec1
Dimitar Atanasov <dimitar.atanasov@limechain.tech>
2024-11-06 19:49:29 +0200

    Refactor

--- src/components/metadataBuilders/voidBuilder/voidBuilder.tsx
+++ src/components/metadataBuilders/voidBuilder/voidBuilder.tsx
@@ -1,14 +1,17 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect } from 'react';
+import { useRef } from 'react';
 
 import type { InvocationOnChangeProps } from '@components/invocationArgsMapper/types';
 
-interface IVoidBuilder extends InvocationOnChangeProps {}
+interface IVoidBuilder extends InvocationOnChangeProps { }
 
 const VoidBuilder = ({ onChange }: IVoidBuilder) => {
-  useEffect(() => {
+  const hasCalledOnChange = useRef(false);
+
+  if (!hasCalledOnChange.current) {
     onChange(undefined);
-  }, []);
+    hasCalledOnChange.current = true;
+  }
 
   return null;
 };
